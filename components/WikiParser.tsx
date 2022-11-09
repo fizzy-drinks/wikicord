72c2c959676da4ea74f30d311aaf7dd06195a91b
fizzy <fizzy@lins.dev>
2022-11-09 13:00:51 -0300

    remove special link syntax

--- components/WikiParser.tsx
+++ components/WikiParser.tsx
@@ -3,7 +3,6 @@ import ReactMarkdown from "react-markdown";
 import rehypePrism from "rehype-prism-plus";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import enhanceWikiLinks from "utils/enhanceWikiLinks";
 import remarkSectionize from "remark-sectionize";
 import rehypeToc from "@jsdevtools/rehype-toc";
 
@@ -17,7 +16,7 @@ const WikiParser: FC<{ children: string }> = ({ children }) => {
         [rehypePrism, { ignoreMissing: true, showLineNumbers: true }],
       ]}
     >
-      {enhanceWikiLinks(children)}
+      {children}
     </ReactMarkdown>
   );
 };
