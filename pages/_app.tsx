50c408e757181beccb1e51a393a1163a34cdafcc
fizzy <fizzy@lins.dev>
2022-11-08 23:27:48 -0300

    use merriweather

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from "next";
 import { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const Styles = createGlobalStyle`
+  @import url('https://fonts.cdnfonts.com/css/merriweather');
+
+  html {
+    font-family: Merriweather, serif;
+    line-height: 1.5;
+  }
+`;
 
 const WikicordApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
-    <div style={{ fontFamily: "sans-serif", lineHeight: 1.5 }}>
+    <>
+      <Styles />
       <Component {...pageProps} />
-    </div>
+    </>
   );
 };
 
