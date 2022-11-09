0ee3b5baae99c977c3a598a096cba574d7ac5a1b
fizzy <fizzy@lins.dev>
2022-11-08 23:19:53 -0300

    fix mobile header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,24 +2,30 @@ import { Guild } from "discord.js";
 import Link from "next/link";
 import { FC } from "react";
 import SearchBar from "./SearchBar";
+import styled from "styled-components";
+
+const TopNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #eee;
+  padding: 0.5rem;
+
+  @media (max-width: 840px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
 
 const Header: FC<{ guild: Guild }> = ({ guild }) => {
   return (
     <header>
-      <nav
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          backgroundColor: "#eee",
-          padding: "0.5rem",
-        }}
-      >
+      <TopNav>
         <h1 style={{ margin: 0 }}>{guild.name} wiki</h1>
         <Link href={`/${guild.id}/wiki/Home_Page`}>Home page</Link>
         <Link href={`/${guild.id}`}>Summary</Link>
         <SearchBar guild={guild} query="" />
-      </nav>
+      </TopNav>
     </header>
   );
 };
