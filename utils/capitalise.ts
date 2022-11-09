6c4bb7a4199f4fd5e3954a5286fa5d68a491cfa8
fizzy <fizzy@lins.dev>
2022-11-09 08:24:27 -0300

    fix capitalise function

--- utils/capitalise.ts
+++ utils/capitalise.ts
@@ -1,5 +1,5 @@
 const capitalise = (phrase: string) => {
-  const spaced = phrase.replace("_", " ").toLowerCase();
+  const spaced = phrase.replace(/_/g, " ").toLowerCase();
   return spaced[0].toUpperCase().concat(spaced.slice(1));
 };
 
